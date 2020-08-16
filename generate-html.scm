2820caed4273b30f0d18aab38a7a789d4c1a1d0d
Jaeho Lee <jaeho.lee@snu.ac.kr>
2020-08-16 06:46:59 +0000

    :construction: Working on HTML generation

--- generate-html.scm
+++ generate-html.scm
@@ -1,3 +1,65 @@
+;;;; Utils
+; Put `delim` after each element in list `l`.
+(define (put-after delim l)
+  (fold-right
+    (lambda (hd tl)
+      (cons hd (cons delim tl)))
+    '()
+    l))
+
+(define (string-replace contents from to)
+  (define (from->to contents)
+    (string-replace contents from to))
+  (let ((from-len (string-length from)))
+    (cond ((string-null? contents) "")
+          ((string-prefix? from contents)
+           (string-append
+             to
+             (from->to (string-tail contents from-len))))
+          (else
+            (string-append
+              (string-head contents 1)
+              (from->to (string-tail contents 1)))))))
+
+;;;; HTML Stuffs
+(define html-decl "<!DOCTYPE html>")
+
+(define (start tag)
+  (string-append "<" (symbol->string tag) ">"))
+(define (end tag)
+  (string-append "</" (symbol->string tag) ">"))
+
+(define (env tag . contents)
+  (apply
+    string-append
+    (put-after
+      "\n"
+      (cons (start tag)
+            (append contents
+                    (list (end tag)))))))
+
+; HTML tags
+(define (html head body) (env 'html head body))
+(define (head x) (env 'head x))
+(define (title x) (env 'title x))
+(define (body x) (env 'body x))
+(define (p x) (env 'p x))
+(define (i x) (env 'i x))
+(define (tt x) (env 'tt x))
+(define br (start 'br))
+
+(define (html-document t b)
+  (string-append
+    html-decl
+    "\n"
+    (html
+      (head (title t))
+      (body b))))
+
+(define (newline-><br> s)
+  (string-replace s "\n" "<br>\n"))
+
+;;;;; File I/O
 ; The list of all files in `directory`.
 (define (files-in directory)
   (filter
@@ -16,12 +78,25 @@
                   (loop (read-char input-port)))
             '())))))
 
-; The content of `file` as a string.
-(define (file->string file)
+; Read `file` as a string.
+(define (read-file file)
   (list->string (file->list file)))
 
+; Write `string` to `file`.
+(define (write-file string file)
+  (call-with-output-file
+    file
+    (lambda (output-port)
+      (display string output-port))))
+
 ; test code
-(display
-  (file->string
+(define test-file
+  (read-file
     (car (files-in "chapter-2/"))))
-(newline)
+
+(define test-html
+  (html-document
+    "Sample HTML"
+    (p (tt (newline-><br> test-file)))))
+
+(write-file test-html "test-html.html")
